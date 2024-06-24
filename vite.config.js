7ddd609cec931e70720af16513f03eb136547248
Tretiak-Anna <tretiakann@gmail.com>
2024-06-24 22:20:40 +0300

    fixed

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "modern-normalize";
-import { Provider } from "react-redux";
-import App from "./components/App/App";
-import { store } from "./redux/store";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-import "./index.css";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
-);
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    sourcemap: true,
+  },
+});
